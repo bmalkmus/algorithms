db319e423414934d157f691e23a4046df2725b7f
bmalkmus <bmalkmus23@gmail.com>
2020-10-13 23:22:37 -0700

    bubble sort

---
+++ algorithms/bubble.js
@@ -0,0 +1,20 @@
+function bubble(arr) {
+    let swapped
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i+1]){
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }
+        }
+    }
+    while(swapped);
+    return arr
+}
+
+console.log(bubble([23,322,123,12,12,23213,435,654,567,8678,435,123,1,34,5,545,66,4]))
+
+
